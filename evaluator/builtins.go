8aa4e250ceb69e004a621b4ae0ecb2a08e73238c
alanxmay <alan.xmay@gmail.com>
2022-05-13 22:21:29 +0800

    add builtin function `string_join`

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,6 +3,7 @@ package evaluator
 import (
 	"fmt"
 	"my-compiler/object"
+	"strings"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -110,6 +111,30 @@ var builtins = map[string]*object.Builtin{
 				fmt.Println(arg.Inspect())
 			}
 
+			return NULL
+		},
+	},
+	"string_join": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument[0] to `string_join` must be array, got=%s", args[0].Type())
+			}
+			if args[1].Type() != object.STRING_OBJ {
+				return newError("argument[1] to `string_join` must be string, got=%s", args[1].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			sep := args[1].(*object.String)
+			if len(arr.Elements) > 0 {
+				arr_string := make([]string, int64(len(arr.Elements)))
+				for i, elem := range arr.Elements {
+					arr_string[i] = (elem.(*object.String)).Value
+				}
+				return &object.String{Value: strings.Join(arr_string, sep.Value)}
+			}
 			return NULL
 		},
 	},
